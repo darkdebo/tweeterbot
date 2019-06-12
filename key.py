869005c8af88ba6eb13daaa792eda98a9133bf8b
darkdebo <50295688+darkdebo@users.noreply.github.com>
2019-06-12 17:17:15 +0530

    Update key.py

--- key.py
+++ key.py
@@ -1,6 +1,6 @@
 # Twitter application consumer keys and access tokens.
 # Remember, keep this information private.
-consumer_key = 'JZydoW1KR2Cd0fCd3b6HdWMkf'
-consumer_secret = 'A0xtYx6aluLQLMVGbPA7vQ7Lihvh1VqQ7R1A2V6VfsieHPzdty '
+ Api_key= 'JZydoW1KR2Cd0fCd3b6HdWMkf'
+ Api_secret_key= 'A0xtYx6aluLQLMVGbPA7vQ7Lihvh1VqQ7R1A2V6VfsieHPzdty '
 access_token = '1132851640057225216-dN2VSjqwTnR3a6TehyN12f4utFSFNO'
 access_token_secret = 'gvYnj9HLYtewqKyatct4EcrWYb0LTWsr1yS0flXc3edbv'
